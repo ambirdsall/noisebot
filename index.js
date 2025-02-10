f187e60133497b0fa165840263ec5c7eaba21762
Alex Birdsall <ambirdsall@gmail.com>
2025-02-10 08:42:50 -0800

    Make index.js a key-listening server process

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
+const readline = require('readline/promises')
 const {
   DeviceDiscovery,
   AsyncDeviceDiscovery,
 } = require('sonos')
 
 const snoop = new AsyncDeviceDiscovery()
+
+const listenForKeys = (keymap) => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  })
+
+  const onKeypress = (key, data) => {
+    const keybind = keymap[key] || keymap[key.toLowerCase()]
+
+    // add a newline so the user-entered character is separated from the
+    // command output
+    console.log("")
+
+    if (typeof keybind === 'function') keybind()
+  }
+
+  rl.input.on('keypress', onKeypress)
+  return rl
+}
 
 async function main() {
   let devices = await snoop.discoverMultiple()
@@ -19,7 +40,16 @@ async function main() {
       [speaker.deets.roomName]: speaker
     }), {})
 
-  console.log(Object.keys(speakers))
+  listenForKeys({
+    l() { console.log(Object.keys(speakers)) },
+    r() { console.log(`pretend I'm Jupiter and I'm selecting the line in for the record player, grouped with some other speakers`) },
+    q() { process.exit(0) },
+  })
+
+  console.log('listening')
+
+  // keep it looping forever
+  setInterval(() => {}, 1 << 30)
 }
 
-main().then(() => process.exit())
+main()
